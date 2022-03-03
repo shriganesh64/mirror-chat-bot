582cfec1f038519faaa9202816b17ce01c364acd
Shri Ganesh S Holebagil <shriganesh64@gmail.com>
2022-03-03 13:19:34 +0530

    File structure changes and code cleanup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import { ThemeProvider } from "@mui/material";
 import AllChatContainer from "./Components/AllChatContainer";
-import theme from "./Theme/themes";
 
 function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <AllChatContainer />
-    </ThemeProvider>
-  );
+  return <AllChatContainer />;
 }
 
 export default App;
